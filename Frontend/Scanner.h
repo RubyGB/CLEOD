2408dfe0fae3045e704d761aac7527ca1cedf408
Jonathan Keeler <15jwk1@caslab.queensu.ca>
2019-10-22 13:28:05 -0400

    Added some skeleton functions to Scanner.h

--- Frontend/Scanner.h
+++ Frontend/Scanner.h
@@ -5,9 +5,21 @@
 #ifndef CLEOD_SCANNER_H
 #define CLEOD_SCANNER_H
 
+#include <string>
+#include <fstream>
+
+#include "Token.h"
 
 class Scanner {
+private:
+    //  scan() should fill this up and return a copy of it.
+    std::vector<Token> tokens;
 
+    //  Constructor should initialize this.
+    std::ifstream src;
+public:
+    Scanner(const std::string &sourceFileName);
+    std::vector<Token> scan();
 };
 
 
