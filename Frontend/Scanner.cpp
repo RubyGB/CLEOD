9e8dbce7e3d367c26de83b43ef6c15e2f150c5ad
Andrew Ma <andrewma@Andrews-MacBook-Pro.local>
2019-10-22 15:09:17 -0400

    declared functions and variables needed for scanTokens() function.

--- Frontend/Scanner.cpp
+++ Frontend/Scanner.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Scanner.h"
+
+std::vector<Token> Scanner::scanTokens() {
+    while(!isAtEnd()){
+        start = current;
+        scanToken();
+    }
+    tokens.push_back({TokenType::EF,  "", line});
+    return tokens;
+}
+Scanner::Scanner(const std::string &sourceFileName) {
+    source = sourceFileName;
+}
+
+// Helper functions for scanTokens()
+bool Scanner::isAtEnd() {
+    return current >= source.length();
+}
+
+void Scanner::scanToken() {
+
+}
--- Frontend/Scanner.h
+++ Frontend/Scanner.h
@@ -12,14 +12,28 @@
 
 class Scanner {
 private:
+    // Private variables
+    int start = 0;
+    int current = 0;
+    uint64_t line = 0;
+    char c;
     //  scan() should fill this up and return a copy of it.
+    std::string source;
     std::vector<Token> tokens;
 
     //  Constructor should initialize this.
     std::ifstream src;
+    bool isAtEnd();
+    void scanToken();
+    char advance();
+    void addToken(TokenType type);
+    void addToken(TokenType type, std::string data);
+
 public:
+    // Constructor for Scanner class
     Scanner(const std::string &sourceFileName);
-    std::vector<Token> scan();
+    std::vector<Token> scanTokens();
+
 };
 
 
