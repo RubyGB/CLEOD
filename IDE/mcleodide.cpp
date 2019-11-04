98e51d0af69de0b680edff0cd0ebf5c18399f8e1
ericchen0514 <53470660+ericchen0514@users.noreply.github.com>
2019-11-03 20:31:36 -0500

    Adding some Button Action Listeners

--- IDE/mcleodide.cpp
+++ IDE/mcleodide.cpp
@@ -6,6 +6,7 @@ McleodIDE::McleodIDE(QWidget *parent)
     , ui(new Ui::McleodIDE)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
 }
 
 McleodIDE::~McleodIDE()
@@ -13,3 +14,94 @@ McleodIDE::~McleodIDE()
     delete ui;
 }
 
+
+void McleodIDE::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void McleodIDE::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this,"Warning","Cannot open file: "+ file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void McleodIDE::on_actionSave_triggered()
+{
+    QString fileName;
+    // If we don't have a filename from before, get one.
+    if (currentFile.isEmpty()) {
+        fileName = QFileDialog::getSaveFileName(this, "Save");
+        currentFile = fileName;
+    } else {
+        fileName = currentFile;
+    }
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void McleodIDE::on_actionSave_as_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void McleodIDE::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+void McleodIDE::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void McleodIDE::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void McleodIDE::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void McleodIDE::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void McleodIDE::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
--- IDE/mcleodide.h
+++ IDE/mcleodide.h
@@ -2,6 +2,11 @@
 #define MCLEODIDE_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QTabWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class McleodIDE; }
@@ -15,7 +20,29 @@ public:
     McleodIDE(QWidget *parent = nullptr);
     ~McleodIDE();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
 private:
     Ui::McleodIDE *ui;
+    QString currentFile = "";
 };
 #endif // MCLEODIDE_H
